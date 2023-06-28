c3186b9c7d159c5aab11ac75734b4354a88d2d86
Rahul Babar <rahulbabar1212@gmail.com>
2023-06-28 12:37:59 +0530

    hello

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -3,9 +3,21 @@ import '../css/Login.css'
 
 const Login = () => {
   return (
-    <div>
-      Login Here
+
+    <div className='black'>
+      <div className='blur'>
+        <img src='City driver-pana (1).png' className='car'></img>
+
+        <form className='flex'>
+          <label for='email'>User ID/Email ID</label>
+          <input type='text' name='email' placeholder='your username' className='user'></input>
+          
+        </form>
+
+      </div>
+
     </div>
+
   )
 }
 
