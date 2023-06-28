a90e643c44ca61b2fb234760c473d19e1f32f6ab
Rahul Babar <rahulbabar1212@gmail.com>
2023-06-28 21:26:04 +0530

    updated

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -3,20 +3,27 @@ import '../css/Login.css'
 
 const Login = () => {
   return (
-
-    <div className='black'>
-      <div className='blur'>
-        <img src='City driver-pana (1).png' className='car'></img>
-
-        <form className='flex'>
-          <label for='email'>User ID/Email ID</label>
-          <input type='text' name='email' placeholder='your username' className='user'></input>
-          
-        </form>
-
+    <div className='blur'>
+      <div className='black'>
+        <div className='img'>
+          <img src='City driver-pana (1).png' className='imga'></img>
+        </div>
+        <div className='input'>
+          <form>
+            <h1>Email ID</h1>
+            <input type='text' className='emailid' ></input>
+          </form>
+          <form>
+            <h1>Email ID</h1>
+            <input type='text' className='emailid' ></input>
+          </form>
+        </div>
+        <div className='buttons'>
+        <button class="button">Button</button>
+        </div>
       </div>
-
     </div>
+
 
   )
 }
