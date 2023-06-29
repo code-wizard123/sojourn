626a4bb305d526a13eaaf1c6ff5f4d161afcbe5a
Rahul Babar <rahulbabar1212@gmail.com>
2023-06-29 12:14:01 +0530

    done

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -8,18 +8,31 @@ const Login = () => {
         <div className='img'>
           <img src='City driver-pana (1).png' className='imga'></img>
         </div>
+
         <div className='input'>
-          <form>
-            <h1>Email ID</h1>
+          <div className='center'>
+          <form >
+            <h1 >Email ID</h1>
             <input type='text' className='emailid' ></input>
           </form>
+          </div>
+          <div className='center'>
           <form>
             <h1>Email ID</h1>
             <input type='text' className='emailid' ></input>
           </form>
         </div>
+        </div>
         <div className='buttons'>
-        <button class="button">Button</button>
+        <button class="button">Sign In</button>
+        </div>
+
+        <div className='centr'>
+          <p>Don’t know your password ?<a href='https://www.freepik.com/free-vector/travelers-concept-illustration_8793245.htm#query=tourist&position=23&from_view=keyword&track=sph'><span>Forgot password</span></a></p>
+        </div>
+
+        <div className='buttons1'>
+        <button class="button1">Register</button>
         </div>
       </div>
     </div>
