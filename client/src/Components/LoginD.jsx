defee0b2f3f184c0e926fc2bad8efb66f7c7d6f1
Raunak Singh Kalsi <98894054+code-wizard123@users.noreply.github.com>
2023-06-30 15:20:10 +0530

    Dark Mode CSS

--- client/src/Components/LoginD.jsx
+++ client/src/Components/LoginD.jsx
@@ -9,35 +9,32 @@ const LoginD = () => {
           <div className={LoginDstyles.img}>
             <img src='./Images/Digital_nomad-pana.png' className={LoginDstyles.imga}></img>
           </div>
-
-          <div className={LoginDstyles.input}>
-            <div className={LoginDstyles.center}>
-              <form >
+          <form>
+            <div className={LoginDstyles.input}>
+              <div className={LoginDstyles.center}>
                 <h1 >Email ID</h1>
                 <input type='text' className={LoginDstyles.emailid} ></input>
-              </form>
-            </div>
-            <div className={LoginDstyles.center}>
-              <form>
+              </div>
+              <div className={LoginDstyles.center}>
                 <h1>Password</h1>
                 <input type='text' className={LoginDstyles.emailid} ></input>
-              </form>
+              </div>
             </div>
-          </div>
-          <div className={LoginDstyles.buttons}>
-            <button class={LoginDstyles.button}>Sign In</button>
-          </div>
+            <div className={LoginDstyles.buttons}>
+              <button class={LoginDstyles.button}>Sign In</button>
+            </div>
 
-          <div className={LoginDstyles.centr}>
-            <p>Don't know your password ?<a href="#"><span>Forgot password</span></a></p>
-          </div>
+            <div className={LoginDstyles.centr}>
+              <p>Don't know your password ?<a href="#"><span>Forgot password</span></a></p>
+            </div>
 
-          <div className={LoginDstyles.buttons1}>
-            <button class={LoginDstyles.button1}>Register</button>
-          </div>
+            <div className={LoginDstyles.buttons1}>
+              <button class={LoginDstyles.button1}>Register</button>
+            </div>
+          </form>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
