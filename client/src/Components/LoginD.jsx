060abd2834033a30d5e8d04208cb8d3a0e3468a7
Rahul Babar <rahulbabar1212@gmail.com>
2023-06-30 15:21:23 +0530

    1

--- client/src/Components/LoginD.jsx
+++ client/src/Components/LoginD.jsx
@@ -9,32 +9,33 @@ const LoginD = () => {
           <div className={LoginDstyles.img}>
             <img src='./Images/Digital_nomad-pana.png' className={LoginDstyles.imga}></img>
           </div>
+          <form >
+            <div className={LoginDstyles.input}>
+              <div className={LoginDstyles.center}>
 
-          <div className={LoginDstyles.input}>
-            <div className={LoginDstyles.center}>
-              <form >
                 <h1 >Email ID</h1>
                 <input type='text' className={LoginDstyles.emailid} ></input>
-              </form>
-            </div>
-            <div className={LoginDstyles.center}>
-              <form>
+
+              </div>
+              <div className={LoginDstyles.center}>
+
                 <h1>Password</h1>
                 <input type='text' className={LoginDstyles.emailid} ></input>
-              </form>
+
+              </div>
             </div>
-          </div>
-          <div className={LoginDstyles.buttons}>
-            <button class={LoginDstyles.button}>Sign In</button>
-          </div>
+            <div className={LoginDstyles.buttons}>
+              <button class={LoginDstyles.button}>Sign In</button>
+            </div>
 
-          <div className={LoginDstyles.centr}>
-            <p>Don't know your password ?<a href="#"><span>Forgot password</span></a></p>
-          </div>
+            <div className={LoginDstyles.centr}>
+              <p>Don't know your password ?<a href="#"><span>Forgot password</span></a></p>
+            </div>
 
-          <div className={LoginDstyles.buttons1}>
-            <button class={LoginDstyles.button1}>Register</button>
-          </div>
+            <div className={LoginDstyles.buttons1}>
+              <button class={LoginDstyles.button1}>Register</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
