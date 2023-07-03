6fc2e73e0e9dc4bb9d160f898e11643c8edff763
Rahul Babar <rahulbabar1212@gmail.com>
2023-07-03 13:03:14 +0530

    no changes

--- client/src/Components/Landing.jsx
+++ client/src/Components/Landing.jsx
@@ -7,17 +7,17 @@ const Landing = () => {
 
     <div className="App">
       <Parallax pages={2} style={{ top: '0', left: '0' }} class="animation">
-        <ParallaxLayer offset={0} speed={0.25}>
-          <img src='./Images/2829.png' className={LandingStyles.imga}></img>
+        <ParallaxLayer offset={0} speed={0.4}>
+          <img src='./Images/3d-render-mountains-against-space-sky.jpg' className={LandingStyles.imga}></img>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.3}>
-          <img src='./Images/2.png' className={LandingStyles.imgb}></img>
-          <img src='./Images/4.png' className={LandingStyles.imgc}></img>
+          {/* <img src='./Images/2.png' className={LandingStyles.imgb} id='imgb'></img>
+          <img src='./Images/4.png' className={LandingStyles.imgc}></img> */}
           {/* <img src='./Images/3.png' className={LandingStyles.imgd}></img> */}
-          <img src='./Images/5.png' className={LandingStyles.imge}></img>
-          <img src='./Images/1.png' className={LandingStyles.imgf}></img>
+          {/* <img src='./Images/5.png' className={LandingStyles.imge}></img>
+          <img src='./Images/1.png' className={LandingStyles.imgf}></img> */}
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.9}>
+        <ParallaxLayer offset={0} speed={0.5}>
      <h1 className={LandingStyles.h1}>SOJOURN</h1>
      <h2 className={LandingStyles.h2}>Book Travell Enjoy</h2>
         </ParallaxLayer>
