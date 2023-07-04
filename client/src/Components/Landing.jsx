f61f2d78d97d3e82c488bdf7665ffcf1b60e14b2
Rahul Babar <rahulbabar1212@gmail.com>
2023-07-04 15:10:28 +0530

    responsive landing pages

--- client/src/Components/Landing.jsx
+++ client/src/Components/Landing.jsx
@@ -29,7 +29,7 @@ const Landing = () => {
 The Landing page  features beautiful images showcasing the website. Major upcoming events and festivals helps visitors plan their trip. Useful links for things like transportation, accommodations and dining options are prominently displayed.
 <br/><br/>The Sojourn tourism website should provide a warm welcome  for all visitors. The website represents the city and its people. The primary goal is to make visitors’ stays as enjoyable and memorable as possible.
 The Landing page  features beautiful images showcasing the website. Major upcoming events and festivals helps visitors plan their trip. Useful links for things like transportation, accommodations and dining options are prominently displayed.
-<br/><br/>Rahul Babar - <a href="https://github.com/rahulbabar1212">GitHub</a><br/>Ranauk Singh Kalsi - <a href="https://github.com/code-wizard123">GitHub</a></h1>
+<br/><br/>Rahul Babar - <a href="">GitHub</a><br/>Ranauk Singh Kalsi - <a href="">GitHub</a></h1>
         </div>
 
         </ParallaxLayer>
@@ -41,8 +41,4 @@ The Landing page  features beautiful images showcasing the website. Major upcomi
 }
 
 export default Landing
-{/* <div>
-<h1>
-  hello
-</h1>
-</div> */}
+// https://github.com/rahulbabar1212 , https://github.com/code-wizard123
