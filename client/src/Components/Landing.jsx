c6bebca9112287968eaf27b2a9640442ee7a90ad
Raunak Singh Kalsi <98894054+code-wizard123@users.noreply.github.com>
2023-07-04 12:36:07 +0530

    Landing Login fix

--- client/src/Components/Landing.jsx
+++ client/src/Components/Landing.jsx
@@ -4,22 +4,20 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 const Landing = () => {
   return (
-
-    <div className="App">
+    <div className={LandingStyles.App}>
       <Parallax pages={2} style={{ top: '0', left: '0' }} class="animation">
         <ParallaxLayer offset={0} speed={0.1}>
-          <img src='./Parallax-images/cloud-back.jpg' className={LandingStyles.imga}></img>
+          <img src='./Parallax-images/cloud-back.jpg' className={`${LandingStyles.imga} ${LandingStyles.landimg}`}></img>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={-0.5}>
-          <img src='./Parallax-images/moon.png' className={LandingStyles.imgb}></img>
+          <img src='./Parallax-images/moon.png' className={`${LandingStyles.imgb} ${LandingStyles.landimg}`}></img>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.2}>
-          <img src='./Parallax-images/3.png' className={LandingStyles.imgc}></img>
+          <img src='./Parallax-images/3.png' className={`${LandingStyles.imgc} ${LandingStyles.landimg}`}></img>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.4}>
-          <img src='./Parallax-images/2.png' className={LandingStyles.imgc}></img>
+          <img src='./Parallax-images/2.png' className={`${LandingStyles.imgc} ${LandingStyles.landimg}`}></img>
         </ParallaxLayer>
-
         <ParallaxLayer offset={0} speed={0.9}>
           <h1 className={LandingStyles.h1}>SOJOURN</h1>
           <h2 className={LandingStyles.h2}>Book Travel Enjoy</h2>
